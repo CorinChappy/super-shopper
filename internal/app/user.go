f034cf221f9a17c1870d3dbba27703facdea1dd8
Corin Chaplin <git@corinchaplin.co.uk>
2018-08-06 23:09:02 +0100

    Support for getting individual users by ID

--- internal/app/user.go
+++ internal/app/user.go
@@ -2,5 +2,30 @@ package main
 
 // User contains the information about a user
 type User struct {
-	ID int `json:"ID"`
+	ID       int    `json:"ID"`
+	Username string `json:"username"`
+}
+
+func rowToUser(r Scannable) (*User, error) {
+	user := User{}
+
+	err := r.Scan(&user.ID, &user.Username)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+// GetUser returns the information about the user parameter
+func GetUser(userID int) (*User, error) {
+	db := GetDb()
+
+	stmt, err := db.Prepare("SELECT ID, username FROM User WHERE ID = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	return rowToUser(stmt.QueryRow(userID))
 }
--- internal/app/v1Router.go
+++ internal/app/v1Router.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,9 +10,24 @@ import (
 // V1Router sets up routes for the v1 of the api
 func V1Router(g *gin.RouterGroup) {
 	g.GET("/healthcheck", healthCheck)
+	g.GET("/user/:userId", getUser)
 }
 
 func healthCheck(c *gin.Context) {
 	q := c.Query("q")
 	c.String(http.StatusOK, "Hello %s", q)
+}
+
+func getUser(c *gin.Context) {
+	userID, err := strconv.Atoi(c.Param("userId"))
+	if err != nil {
+		c.String(http.StatusBadRequest, "Cannot parse userId")
+	}
+
+	user, err := GetUser(userID)
+	if err != nil {
+		c.String(http.StatusBadRequest, "Error getting user %s", err.Error())
+	}
+
+	c.JSON(http.StatusOK, user)
 }
