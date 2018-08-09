22f08191c41556261b204c2ee3a7d9afd675fc41
Corin Chaplin <git@corinchaplin.co.uk>
2018-08-09 19:37:00 +0100

    Add login and signup routes

--- internal/app/user.go
+++ internal/app/user.go
@@ -1,9 +1,24 @@
 package main
 
+import (
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var secret = []byte("SECRET")
+
 // User contains the information about a user
 type User struct {
 	ID       int    `json:"ID"`
 	Username string `json:"username"`
+}
+
+// JwtCustomClaims are the claims stored in a generated jwt
+type JwtCustomClaims struct {
+	User *User `json:"user"`
+	jwt.StandardClaims
 }
 
 func rowToUser(r Scannable) (*User, error) {
@@ -17,8 +32,8 @@ func rowToUser(r Scannable) (*User, error) {
 	return &user, nil
 }
 
-// GetUser returns the information about the user parameter
-func GetUser(userID int) (*User, error) {
+// GetUserByID returns the information about the user parameter
+func GetUserByID(userID int) (*User, error) {
 	db := GetDb()
 
 	stmt, err := db.Prepare("SELECT ID, username FROM User WHERE ID = ?")
@@ -28,4 +43,80 @@ func GetUser(userID int) (*User, error) {
 	defer stmt.Close()
 
 	return rowToUser(stmt.QueryRow(userID))
+}
+
+// GetUserByUsername returns the information about the user parameter
+func GetUserByUsername(username string) (*User, error) {
+	db := GetDb()
+
+	stmt, err := db.Prepare("SELECT ID, username FROM User WHERE username = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	return rowToUser(stmt.QueryRow(username))
+}
+
+// GetToken aquires a token for the given username, as long as the password is valid
+func GetToken(username string, password string) (*User, string, error) {
+	db := GetDb()
+
+	stmt, err := db.Prepare("SELECT ID, username, password FROM User WHERE username = ?")
+	if err != nil {
+		return nil, "", err
+	}
+	defer stmt.Close()
+
+	row := stmt.QueryRow(username)
+	user := User{}
+	var hashedPw string
+	err = row.Scan(&user.ID, &user.Username, &hashedPw)
+	if err != nil {
+		return nil, "", err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPw), []byte(password))
+	if err != nil {
+		return nil, "", err
+	}
+
+	// Generate the gwt
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, JwtCustomClaims{
+		&user,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().AddDate(0, 1, 0).Unix(),
+		},
+	})
+
+	stringToken, err := token.SignedString(secret)
+	if err != nil {
+		return nil, "", err
+	}
+
+	return &user, stringToken, nil
+}
+
+// CreateUser will create the new user and return it if successful
+func CreateUser(username string, password string) (*User, error) {
+	// Generate the hash of the password
+	hashPw, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	db := GetDb()
+
+	stmt, err := db.Prepare("INSERT INTO User (username, password) VALUES (?,?)")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(username, hashPw)
+	if err != nil {
+		return nil, err
+	}
+
+	return GetUserByUsername(username)
 }
--- internal/app/v1Router.go
+++ internal/app/v1Router.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/gin-gonic/gin/binding"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +13,8 @@ import (
 func V1Router(g *gin.RouterGroup) {
 	g.GET("/healthcheck", healthCheck)
 	g.GET("/user/:userId", getUser)
+	g.POST("/login", login)
+	g.POST("/signup", signup)
 }
 
 func healthCheck(c *gin.Context) {
@@ -24,10 +28,56 @@ func getUser(c *gin.Context) {
 		c.String(http.StatusBadRequest, "Cannot parse userId")
 	}
 
-	user, err := GetUser(userID)
+	user, err := GetUserByID(userID)
 	if err != nil {
 		c.String(http.StatusBadRequest, "Error getting user %s", err.Error())
 	}
 
 	c.JSON(http.StatusOK, user)
+}
+
+type loginParams struct {
+	Username string `form:"username" json:"username" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
+
+func login(c *gin.Context) {
+	var json loginParams
+
+	err := c.ShouldBindWith(&json, binding.JSON)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Try to obtain a token
+	user, token, err := GetToken(json.Username, json.Password)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"token": token,
+		"user":  user,
+	})
+}
+
+func signup(c *gin.Context) {
+	// We can reuse login for signup
+	var json loginParams
+
+	err := c.ShouldBindWith(&json, binding.JSON)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := CreateUser(json.Username, json.Password)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, user)
 }
